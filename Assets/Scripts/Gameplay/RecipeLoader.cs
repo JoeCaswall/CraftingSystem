d5e9ebb90bf94a549ae7432b3a22b92a01251eef
Joe Caswall <joe.caswall@vodafone.com>
2025-11-10 17:23:41 +0000

    Added defaultexecutionorder to force recipeloader to run before crafting station behaviour

--- Assets/Scripts/Gameplay/RecipeLoader.cs
+++ Assets/Scripts/Gameplay/RecipeLoader.cs
@@ -6,16 +6,17 @@ using UnityScripts;
 
 namespace Gameplay
 {
+    [DefaultExecutionOrder(-100)]
     public class RecipeLoader : MonoBehaviour
     {
         [SerializeField] private List<RecipeSO> recipeAssets;
-
         void Awake()
         {
             foreach (var recipeSO in recipeAssets)
             {
                 Recipe runtimeRecipe = CraftingStationBehaviour.ConvertToRecipe(recipeSO);
-
+                Debug.Log(runtimeRecipe.Name);
+        
                 if (runtimeRecipe is ItemRecipe itemRecipe)
                     RecipeRegistry.RegisterItemRecipe(itemRecipe);
                 else if (runtimeRecipe is MaterialRecipe materialRecipe)
